afee49ef6b9fe106fb8bef45839d93ccde95b2e6
Jacopo Ghisleni <jacopo.ghisleni@gmail.com>
2025-01-08 09:11:51 +0100

    Update 8_Allocazioni Sede.py

--- pages/8_Allocazioni Sede.py
+++ pages/8_Allocazioni Sede.py
@@ -12,6 +12,9 @@ if "road_data" not in st.session_state:
 if "calculated" not in st.session_state:
     st.session_state.calculated = False
 
+if "allocation_started" not in st.session_state:
+    st.session_state.allocation_started = False
+
 # Tabs principali
 tab1, tab2 = st.tabs(["📂 Caricamento File", "📊 Risultati Analisi"])
 
@@ -80,68 +83,101 @@ with tab2:
     st.header("Risultati Analisi")
 
     if st.session_state.road_data is not None:
-        if not st.session_state.calculated:
-            st.write("Premi il pulsante per calcolare i risultati.")
-            
-            if st.button("Calcola Allocazione"):
-                # Esegue il codice originale per l'allocazione
-                Road = st.session_state.road_data
-                Province = st.session_state.province_data
-                edited_sedi = st.session_state.sedi_data
+        if st.button("Avvia Allocazione"):
+            st.session_state.allocation_started = True
+            st.success("Codice di allocazione avviato!")
 
-                for index, row in Road.iterrows():
-                    valore_cercato = row['Path_originale']
-                    if valore_cercato in Province['Path'].values:
-                        Road.at[index, 'Distanza Baseline'] = Province.loc[Province['Path'] == valore_cercato, 'Distanza km'].values[0]
-                        Road.at[index, 'Tempo Baseline'] = Province.loc[Province['Path'] == valore_cercato, 'Durata minuti'].values[0]
-                    else:
-                        Road.at[index, 'Distanza Baseline'] = None
-                        Road.at[index, 'Tempo Baseline'] = None
+        if st.session_state.allocation_started:
+            if not st.session_state.calculated:
+                if st.button("Calcola Allocazione"):
+                    # Esegue il codice originale per l'allocazione
+                    Road = st.session_state.road_data
+                    Province = st.session_state.province_data
+                    Configurazione_sedi = st.session_state.sedi_data
+                    # Calcolo i valori dello scenario di riferimento
+                    for colonna in Configurazione_sedi.columns[1:]:
+                        nome_colonna=colonna
+                        # Definisco le colonne di salvataggio
+                        colonna1=colonna+'- Distanza Km'
+                        colonna2=colonna+'- Tempo '
+                        colonna3=colonna+'- Sede Riferimento'
+                        # Definisco il dataframe per la gestione della colonna
+                        colonna_dataframe=pd.DataFrame(columns=['sede','tipo sede','km','tempo'])
+                
+                        #Utilizzo solo le colonne non nulle
+                        non_null_count=Configurazione_sedi[colonna].count()
+                
+                        try:
+                            # Vincolo sulla partenza
+                            if non_null_count>0:
+                                #Creo la lista di calcolo
+                                for index_1,row_1 in Configurazione_sedi.iterrows():
+                                    value=row_1[colonna] #Recupero la lettera della sede
+                                    try:
+                                        valore_corrispondente=row_1[Configurazione_sedi.columns[0]] #Recupero ad esempio Bergamo
+                                        concatenato=valore_corrispondente+'-'+arrivo # Crea la nuova coppia
+                                    except:
+                                        concatenato='errore'
+                                        
+                                    if concatenato in Province['Path'].values:
+                                        # Trovo il valore corrispondente nella tabella
+                                        colonna_dataframe.at[index_1,'sede']=valore_corrispondente
+                                        colonna_dataframe.at[index_1,'tipo sede']=value
+                                        try:
+                                            colonna_dataframe.at[index_1,'km']=Province.loc[Province['Path']==concatenato,'Distanza km'].values[0]
+                                            colonna_dataframe.at[index_1,'tempo']=Province.loc[Province['Path']==concatenato,'Durata minuti'].values[0]
+                                        except:
+                                            colonna_dataframe.at[index_1,'km']=0
+                                            colonna_dataframe.at[index_1,'tempo']=0
+                
+                                # Filtro il dataframe per delle condizioni sfruttando un case when
+                                if tipo_servizio=='Trade':
+                                    #filtro il dataframe con la condizione A
+                                    colonna_dataframe_filtered=colonna_dataframe[colonna_dataframe['tipo sede']=='A']
+                                    try:
+                                        min_km_row=colonna_dataframe_filtered.loc[colonna_dataframe_filtered['km'].idxmin()]
+                                        Road.at[index,colonna3]=min_km_row['sede']
+                                        Road.at[index,colonna1]=min_km_row['km']
+                                        Road.at[index,colonna2]=min_km_row['tempo']
+                                    except ValueError as e:
+                                        e=1
+                                        #print(arrivo)
+                
+                                else:    
+                                    colonna_dataframe_filtered=colonna_dataframe[(colonna_dataframe['tipo sede']=='A') | (colonna_dataframe['tipo sede']=='B')]
+                                    try:
+                                        print(colonna_dataframe_filtered)
+                                        min_km_row=colonna_dataframe_filtered.loc[colonna_dataframe_filtered['km'].idxmin()]
+                                        Road.at[index,colonna3]=min_km_row['sede']
+                                        Road.at[index,colonna1]=min_km_row['km']
+                                        Road.at[index,colonna2]=min_km_row['tempo']
+                                        print(min_km_row['sede'])
+                                    except ValueError as e:
+                                        e=1
+                                        #print(e)
+                                        #print(arrivo)
+                        except ValueError as e:
+                            print(e)  
+                    
 
-                    # Configurazione e calcolo scenari
-                    for colonna in edited_sedi.columns[1:]:
-                        colonna1 = colonna + '- Distanza Km'
-                        colonna2 = colonna + '- Tempo '
-                        colonna3 = colonna + '- Sede Riferimento'
-                        colonna_dataframe = pd.DataFrame(columns=['sede', 'tipo sede', 'km', 'tempo'])
+                    st.session_state.road_data = Road
+                    st.session_state.calculated = True
+                    st.success("Calcolo completato!")
 
-                        for index_1, row_1 in edited_sedi.iterrows():
-                            value = row_1[colonna]
-                            valore_corrispondente = row_1[edited_sedi.columns[0]]
-                            concatenato = valore_corrispondente + '-' + row['Provincia Arrivo']
-                            if concatenato in Province['Path'].values:
-                                colonna_dataframe.at[index_1, 'sede'] = valore_corrispondente
-                                colonna_dataframe.at[index_1, 'tipo sede'] = value
-                                colonna_dataframe.at[index_1, 'km'] = Province.loc[Province['Path'] == concatenato, 'Distanza km'].values[0]
-                                colonna_dataframe.at[index_1, 'tempo'] = Province.loc[Province['Path'] == concatenato, 'Durata minuti'].values[0]
+            if st.session_state.calculated:
+                st.write("### Tabella con Risultati")
+                st.dataframe(st.session_state.road_data.head(10))
 
-                        colonna_dataframe_filtered = colonna_dataframe[colonna_dataframe['tipo sede'].isin(['A', 'B'])]
-                        if not colonna_dataframe_filtered.empty:
-                            min_km_row = colonna_dataframe_filtered.loc[colonna_dataframe_filtered['km'].idxmin()]
-                            Road.at[index, colonna1] = min_km_row['km']
-                            Road.at[index, colonna2] = min_km_row['tempo']
-                            Road.at[index, colonna3] = min_km_row['sede']
-
-                st.session_state.road_data = Road
-                st.session_state.calculated = True
-                st.success("Calcolo completato!")
-        
-        if st.session_state.calculated:
-            st.write("### Tabella con Risultati")
-            st.dataframe(st.session_state.road_data.head(10))
-
-            # Scaricamento dei risultati
-            output = io.BytesIO()
-            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-                st.session_state.road_data.to_excel(writer, index=False, sheet_name='Risultati')
-            st.download_button(
-                label="Scarica Risultati in Excel",
-                data=output.getvalue(),
-                file_name="Output_Analisi.xlsx",
-                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            )
+                # Scaricamento dei risultati
+                output = io.BytesIO()
+                with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                    st.session_state.road_data.to_excel(writer, index=False, sheet_name='Risultati')
+                st.download_button(
+                    label="Scarica Risultati in Excel",
+                    data=output.getvalue(),
+                    file_name="Output_Analisi.xlsx",
+                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                )
     else:
-        st.warning("Carica i file nella prima tab e premi il pulsante di calcolo.")
-
-
+        st.warning("Carica i file nella prima tab e premi il pulsante di allocazione.")
 
