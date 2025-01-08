4aea16002a161b31258b0f6ccb4c280c8ec0b92d
Jacopo Ghisleni <jacopo.ghisleni@gmail.com>
2025-01-08 08:59:18 +0100

    Update 8_Allocazioni Sede.py

--- pages/8_Allocazioni Sede.py
+++ pages/8_Allocazioni Sede.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import io
 
 # Configurazione della pagina
 st.set_page_config(page_title="Analisi Logistica", layout="wide")
@@ -83,7 +84,7 @@ with tab2:
             st.write("Premi il pulsante per calcolare i risultati.")
             
             if st.button("Calcola Allocazione"):
-                # Esegue il tuo codice originale per l'allocazione
+                # Esegue il codice originale per l'allocazione
                 Road = st.session_state.road_data
                 Province = st.session_state.province_data
                 edited_sedi = st.session_state.sedi_data
@@ -104,7 +105,6 @@ with tab2:
                         colonna3 = colonna + '- Sede Riferimento'
                         colonna_dataframe = pd.DataFrame(columns=['sede', 'tipo sede', 'km', 'tempo'])
 
-                        # Utilizzo delle sedi
                         for index_1, row_1 in edited_sedi.iterrows():
                             value = row_1[colonna]
                             valore_corrispondente = row_1[edited_sedi.columns[0]]
@@ -115,7 +115,6 @@ with tab2:
                                 colonna_dataframe.at[index_1, 'km'] = Province.loc[Province['Path'] == concatenato, 'Distanza km'].values[0]
                                 colonna_dataframe.at[index_1, 'tempo'] = Province.loc[Province['Path'] == concatenato, 'Durata minuti'].values[0]
 
-                        # Filtra e salva
                         colonna_dataframe_filtered = colonna_dataframe[colonna_dataframe['tipo sede'].isin(['A', 'B'])]
                         if not colonna_dataframe_filtered.empty:
                             min_km_row = colonna_dataframe_filtered.loc[colonna_dataframe_filtered['km'].idxmin()]
@@ -123,20 +122,26 @@ with tab2:
                             Road.at[index, colonna2] = min_km_row['tempo']
                             Road.at[index, colonna3] = min_km_row['sede']
 
-                # Salva il calcolo nella sessione
                 st.session_state.road_data = Road
                 st.session_state.calculated = True
                 st.success("Calcolo completato!")
         
-        # Mostra i risultati se il calcolo è stato eseguito
         if st.session_state.calculated:
             st.write("### Tabella con Risultati")
             st.dataframe(st.session_state.road_data.head(10))
 
-            # Opzione per salvare i risultati
-            if st.button("Salva Risultati in Excel"):
-                st.session_state.road_data.to_excel("Output_Analisi.xlsx", index=False)
-                st.success("Risultati salvati come 'Output_Analisi.xlsx'.")
+            # Scaricamento dei risultati
+            output = io.BytesIO()
+            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                st.session_state.road_data.to_excel(writer, index=False, sheet_name='Risultati')
+                writer.save()
+            st.download_button(
+                label="Scarica Risultati in Excel",
+                data=output.getvalue(),
+                file_name="Output_Analisi.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            )
     else:
         st.warning("Carica i file nella prima tab e premi il pulsante di calcolo.")
+
 
