7feac2fd7a51d55acae866ebf5215699b119c186
Jacopo Ghisleni <jacopo.ghisleni@gmail.com>
2025-01-08 09:14:09 +0100

    Update 8_Allocazioni Sede.py

--- pages/8_Allocazioni Sede.py
+++ pages/8_Allocazioni Sede.py
@@ -90,79 +90,40 @@ with tab2:
         if st.session_state.allocation_started:
             if not st.session_state.calculated:
                 if st.button("Calcola Allocazione"):
-                    # Esegue il codice originale per l'allocazione
-                    Road = st.session_state.road_data
-                    Province = st.session_state.province_data
-                    Configurazione_sedi = st.session_state.sedi_data
-                    # Calcolo i valori dello scenario di riferimento
-                    for colonna in Configurazione_sedi.columns[1:]:
-                        nome_colonna=colonna
-                        # Definisco le colonne di salvataggio
-                        colonna1=colonna+'- Distanza Km'
-                        colonna2=colonna+'- Tempo '
-                        colonna3=colonna+'- Sede Riferimento'
-                        # Definisco il dataframe per la gestione della colonna
-                        colonna_dataframe=pd.DataFrame(columns=['sede','tipo sede','km','tempo'])
-                
-                        #Utilizzo solo le colonne non nulle
-                        non_null_count=Configurazione_sedi[colonna].count()
-                
-                        try:
-                            # Vincolo sulla partenza
-                            if non_null_count>0:
-                                #Creo la lista di calcolo
-                                for index_1,row_1 in Configurazione_sedi.iterrows():
-                                    value=row_1[colonna] #Recupero la lettera della sede
-                                    try:
-                                        valore_corrispondente=row_1[Configurazione_sedi.columns[0]] #Recupero ad esempio Bergamo
-                                        concatenato=valore_corrispondente+'-'+arrivo # Crea la nuova coppia
-                                    except:
-                                        concatenato='errore'
-                                        
-                                    if concatenato in Province['Path'].values:
-                                        # Trovo il valore corrispondente nella tabella
-                                        colonna_dataframe.at[index_1,'sede']=valore_corrispondente
-                                        colonna_dataframe.at[index_1,'tipo sede']=value
-                                        try:
-                                            colonna_dataframe.at[index_1,'km']=Province.loc[Province['Path']==concatenato,'Distanza km'].values[0]
-                                            colonna_dataframe.at[index_1,'tempo']=Province.loc[Province['Path']==concatenato,'Durata minuti'].values[0]
-                                        except:
-                                            colonna_dataframe.at[index_1,'km']=0
-                                            colonna_dataframe.at[index_1,'tempo']=0
-                
-                                # Filtro il dataframe per delle condizioni sfruttando un case when
-                                if tipo_servizio=='Trade':
-                                    #filtro il dataframe con la condizione A
-                                    colonna_dataframe_filtered=colonna_dataframe[colonna_dataframe['tipo sede']=='A']
-                                    try:
-                                        min_km_row=colonna_dataframe_filtered.loc[colonna_dataframe_filtered['km'].idxmin()]
-                                        Road.at[index,colonna3]=min_km_row['sede']
-                                        Road.at[index,colonna1]=min_km_row['km']
-                                        Road.at[index,colonna2]=min_km_row['tempo']
-                                    except ValueError as e:
-                                        e=1
-                                        #print(arrivo)
-                
-                                else:    
-                                    colonna_dataframe_filtered=colonna_dataframe[(colonna_dataframe['tipo sede']=='A') | (colonna_dataframe['tipo sede']=='B')]
-                                    try:
-                                        print(colonna_dataframe_filtered)
-                                        min_km_row=colonna_dataframe_filtered.loc[colonna_dataframe_filtered['km'].idxmin()]
-                                        Road.at[index,colonna3]=min_km_row['sede']
-                                        Road.at[index,colonna1]=min_km_row['km']
-                                        Road.at[index,colonna2]=min_km_row['tempo']
-                                        print(min_km_row['sede'])
-                                    except ValueError as e:
-                                        e=1
-                                        #print(e)
-                                        #print(arrivo)
-                        except ValueError as e:
-                            print(e)  
-                    
+                    # Funzione di calcolo
+                    def calcola_allocazione(Road, Province, Configurazione_sedi):
+                        for colonna in Configurazione_sedi.columns[1:]:
+                            colonna1 = colonna + '- Distanza Km'
+                            colonna2 = colonna + '- Tempo '
+                            colonna3 = colonna + '- Sede Riferimento'
 
-                    st.session_state.road_data = Road
-                    st.session_state.calculated = True
-                    st.success("Calcolo completato!")
+                            for index, row in Road.iterrows():
+                                arrivo = row['Provincia Arrivo']
+                                tipo_servizio = row['Tipologia Servizio']
+                                
+                                # Trova le combinazioni di percorso
+                                filtered_province = Province[Province['Provincia Arrivo'] == arrivo]
+                                
+                                if not filtered_province.empty:
+                                    try:
+                                        min_km_row = filtered_province.loc[filtered_province['Distanza km'].idxmin()]
+                                        Road.at[index, colonna1] = min_km_row['Distanza km']
+                                        Road.at[index, colonna2] = min_km_row['Durata minuti']
+                                        Road.at[index, colonna3] = min_km_row['Provincia Origine']
+                                    except ValueError:
+                                        st.warning(f"Errore nell'allocazione per {arrivo}")
+                        return Road
+
+                    # Esegui il calcolo
+                    with st.spinner("Calcolo in corso..."):
+                        Road = calcola_allocazione(
+                            st.session_state.road_data,
+                            st.session_state.province_data,
+                            st.session_state.sedi_data,
+                        )
+                        st.session_state.road_data = Road
+                        st.session_state.calculated = True
+                        st.success("Calcolo completato!")
 
             if st.session_state.calculated:
                 st.write("### Tabella con Risultati")
